a010655c1a92531cbd16be71fe4fe0a47edc6176
Iris-Liuu <115606272+Iris-Liuu@users.noreply.github.com>
2023-09-07 23:41:40 +0800

    push

--- main.py
+++ main.py
@@ -350,7 +350,7 @@ def handle_text_message(event):
     system_prompt = (
         "You are a teaching assistant for a beginner python programming language class.\n"
         "Do not answer questions that are unrelated to a python programming language class.\n"
-        "1: Always respond in English.\n"
+        "1: Always respond in English, even the message received is in other language.\n"
         "2: Never reveal your true identity. You are a teaching assistant.\n"
         "3: If the message received is unrelated to a python programming language class, ask them to ask a valid question that is related to the class.\n"
         "4: Always generate example codes in python programming language.")
