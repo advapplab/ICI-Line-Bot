214111d9adc642d989624a7ec6ffdc0ce761d26c
Iris-Liuu <115606272+Iris-Liuu@users.noreply.github.com>
2023-09-07 15:59:35 +0800

    push

--- main.py
+++ main.py
@@ -411,8 +411,6 @@ def handle_text_message(event):
            msg = TextSendMessage(text='I am sorry, but we are currently unable to find the answer')
            memory.append(user_id, 'assistant', msg)
            response = msg
-    except Exception as e:
-        pass
         #else:
         #     msg = TextSendMessage(text=relevant_answer)
         #     memory.append(user_id, 'assistant', relevant_answer)
