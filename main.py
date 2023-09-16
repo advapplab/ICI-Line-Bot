2223deac467e432f1319e30fdd0fab6c0ef5ec0b
Iris-Liuu <115606272+Iris-Liuu@users.noreply.github.com>
2023-09-16 15:55:05 +0800

    3:54 change the msg form bot

--- main.py
+++ main.py
@@ -435,7 +435,7 @@ def handle_image_message(event):
   img_base64 = image_to_base64(img)
   #store
   store_image(user_id, display_name, user_timestamp, img_base64)
-  msg = TextSendMessage(text='Image stored to the database.')
+  msg = TextSendMessage(text='Image received.')
   line_bot_api.reply_message(event.reply_token, msg)
 
 
