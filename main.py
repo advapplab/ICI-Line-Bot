dc8b32a4f15ef029bcd6adcdade1f1771a0db83d
Iris-Liuu <115606272+Iris-Liuu@users.noreply.github.com>
2023-09-18 10:01:33 +0800

    10:01

--- main.py
+++ main.py
@@ -509,7 +509,8 @@ if __name__ == "__main__":
   #  mongodb.connect_to_database()
   #  storage = Storage(MongoStorage(mongodb.db))
   #else:
-  storage = Storage(FileStorage('db.json'))
+  ## storage = Storage(FileStorage('db.json'))
+  storage = Storage(FileStorage('student_id.json'))
   try:
     data = storage.load()
     for user_id in data.keys():
