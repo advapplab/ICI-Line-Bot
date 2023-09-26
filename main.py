6d65170e643dc3df64e466df8a182b2a0d3395a4
Iris-Liuu <115606272+Iris-Liuu@users.noreply.github.com>
2023-09-26 19:34:55 +0800

    7:34

--- main.py
+++ main.py
@@ -381,7 +381,7 @@ def handle_text_message(event):
       user_id = event.source.user_id
       last_10_documents_list = get_last_10_documents()
       last_message = find_last_message(user_id, last_10_documents_list)
-      if last_message is not None:
+      if last_message == True:
         print(f"Last message sent by user {user_id}: {last_message}")
       else:
         print(f"No previous messages found for user {user_id}")
