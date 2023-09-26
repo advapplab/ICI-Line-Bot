090e2c7a8f29f9e6e7e49b89aeb1546c783252f9
Iris-Liuu <115606272+Iris-Liuu@users.noreply.github.com>
2023-09-26 19:30:50 +0800

    7:30

--- main.py
+++ main.py
@@ -380,7 +380,7 @@ def handle_text_message(event):
     elif text.lower().startswith('/hi'):
       last_10_documents_list = get_last_10_documents()
       last_message = find_last_message(user_id, last_10_documents_list)
-      if last_message:
+      if last_message is not None:
         print(f"Last message sent by user {user_id}: {last_message}")
       else:
         print(f"No previous messages found for user {user_id}")
