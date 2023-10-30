f8800a2119d4343276bb57c16c7d62d7eb87480a
BCarlT <115812341+BCarlT@users.noreply.github.com>
2023-10-31 01:52:33 +0800

    1:52

--- main.py
+++ main.py
@@ -484,7 +484,7 @@ def handle_text_message(event):
           supported_languages = ['en', 'zh', 'th', 'es', 'ja']
           def detect_language_and_respond(user_message, api_key, system_prompt):
             detected_language = detect_language(user_message)
-            if detected_language == 'en':
+            if detected_language == 'en' in supported_languages:
               response = requests.post(
                 'https://api.openai.com/v1/chat/completions',
                   headers = {
