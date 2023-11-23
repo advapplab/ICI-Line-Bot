5fa649755f56a9a8b83203212d60d69e695d0b33
BCarlT <115812341+BCarlT@users.noreply.github.com>
2023-11-24 00:26:52 +0800

    12:26

--- main.py
+++ main.py
@@ -510,7 +510,8 @@ def handle_text_message(event):
                   }
               )
             json = response.json()
-            response = json['choices'][0]['message']['content']
+            return json_response['choices'][0]['message']['content']
+            
           def handle_new_user_message(user_message):
               if is_message_valid(user_message):
                   chat_response = get_chatgpt_response(user_message)
@@ -518,7 +519,7 @@ def handle_text_message(event):
               else:
                   msg = TextSendMessage(text='Please use English to communicate with me or say it again in a complete sentence.')
               return msg
-          msg = TextSendMessage(text=response)
+          #msg = TextSendMessage(text=response)
           # role, response = get_role_and_content(response)
           # msg = TextSendMessage(text=response)
           # memory.append(user_id, role, response)
