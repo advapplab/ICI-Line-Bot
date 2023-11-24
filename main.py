c04298ccab2acebea9d2387e991e4bc1ac2d601d
BCarlT <115812341+BCarlT@users.noreply.github.com>
2023-11-24 16:40:19 +0800

    4:40

--- main.py
+++ main.py
@@ -545,12 +545,9 @@ def handle_text_message(event):
      msg = TextSendMessage(text=str(e)) 
 
   # send out the message
-  @handler.add(MessageEvent, message=TextMessage)
-  def handle_text_message(event):
-    msg = "" 
+  line_bot_api.reply_message(event.reply_token, msg)
   bot_timestamp = int(time.time() * 1000)
   store_history_message(user_id, student_id, text, user_timestamp, msg, bot_timestamp)
-  line_bot_api.reply_message(event.reply_token, msg)
 
 
 ### store images ###
