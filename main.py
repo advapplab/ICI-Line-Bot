e8eccbd9e440fa85ddca1e709bcdbe50ba738007
Iris-Liuu <115606272+Iris-Liuu@users.noreply.github.com>
2024-09-13 00:06:38 +0800

    00:06

--- main.py
+++ main.py
@@ -626,14 +626,14 @@ def handle_text_message(event):
     else:
       msg = TextSendMessage(text=str(e))
 
-  # send out the message
-  bot_timestamp = int(time.time() * 1000)
-  user_id = event.source.user_id  
-  student_data = load_student_data("student_id.json")
-  student_id = student_data[user_id]
-  store_history_message(user_id, student_id, text, user_timestamp, msg, bot_timestamp)
-  line_bot_api.reply_message(event.reply_token, msg)
-  #messaging_api.reply_message(event.reply_token, msg)
+# send out the message
+bot_timestamp = int(time.time() * 1000)
+user_id = event.source.user_id  
+student_data = load_student_data("student_id.json")
+student_id = student_data[user_id]
+store_history_message(user_id, student_id, text, user_timestamp, msg, bot_timestamp)
+line_bot_api.reply_message(event.reply_token, msg)
+#messaging_api.reply_message(event.reply_token, msg)
 
 ### store images ###
 import io
