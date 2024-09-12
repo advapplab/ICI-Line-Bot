28bed14b7b1311fc40dd03b48a800806dab2a208
Iris-Liuu <115606272+Iris-Liuu@users.noreply.github.com>
2024-09-12 22:02:24 +0800

    22:02

--- main.py
+++ main.py
@@ -375,7 +375,7 @@ def handle_text_message(event):
           storage_wrapper.save(users_dict)
           msg = TextSendMessage(
               text=f'Registration successful for student ID: {student_id}')
-      bot_timestamp = int(time.time() * 1000)
+          store_history_message(user_id, student_id, text, user_timestamp, msg, bot_timestamp)
           # if user_id in users_dict:
           #   msg = TextSendMessage(text='You already registered!')  
       else:
@@ -385,6 +385,7 @@ def handle_text_message(event):
               'Invalid registration format. Please use "/register your_student_id"\nEx: /register 123456789'
           )
         line_bot_api.reply_message(event.reply_token, [msg])
+        store_history_message(user_id, student_id, text, user_timestamp, msg, bot_timestamp)
       #   # Save the registration message to the JSON file
       #   users_dict[user_id] = student_id
       #   storage_wrapper.save(users_dict)
